0fc908df3c553a8656bb8e9e603b32a88514bc0d
cassie-s <56046933+cassie-s@users.noreply.github.com>
2022-01-17 14:29:01 -0600

    debugging

--- server/server.js
+++ server/server.js
@@ -9,14 +9,18 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
-// integrate apollo server with express app as middleware
-server.applyMiddleware({ app });
+let apolloServer = null;
+async function startServer() {
+    apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+}
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -33,6 +37,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
   });
 });
